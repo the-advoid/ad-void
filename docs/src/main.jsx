0f22385a1a9985d54d5cb85dd717e543ac506740
igorskyflyer <igor.dvlpr@gmail.com>
2022-08-28 02:22:37 +0200

    Ignore TypeScript check

--- docs/src/main.jsx
+++ docs/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
