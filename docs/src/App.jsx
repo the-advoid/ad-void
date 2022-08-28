d59c7684ba96221751587bafde4613b85db47d7e
igorskyflyer <igor.dvlpr@gmail.com>
2022-08-28 02:19:53 +0200

    Refactor

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
-        <pre>
-            ___        __   _    __        _      __
-           /   |  ____/ /  | |  / /____   (_)____/ /
-          / /| | / __  /   | | / // __ \ / // __  / 
-         / ___ |/ /_/ /    | |/ // /_/ // // /_/ /  
-        /_/  |_|\__,_/     |___/ \____//_/ \__,_/                                       
-        </pre>   
+        <h4>Generator coming soon ☺</h4>
       </div>
     </div>
   )
