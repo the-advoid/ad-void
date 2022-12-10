5ce2b42f88f6bf71d0af870b74654ce1cfc8aefa
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-12-10 17:53:14 +0100

    Update anti-anti-adblock.js

--- scriptlets/anti-anti-adblock.js
+++ scriptlets/anti-anti-adblock.js
@@ -1,6 +1,12 @@
 ##+js(set, hasAdBlocker, false)
 ##+js(set, abp, false)
+
 ##+js(set, isLoadAds, true)
 ##+js(set, generatorAds, true)
 ##+js(set, googleAd, true)
 ##+js(set, zfgloadedpopup, true)
+
+##+js(set, GeneratorAds, function() {})
+##+js(set, Ads_PushPage, function() {})
+##+js(set, Ads_Popunder, function() {})
+##+js(set, Ads_Vignette, function() {})
